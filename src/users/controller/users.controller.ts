d09422794896d58482320f18651c5ab2149d1165
Maaato <50064787+Maaato@users.noreply.github.com>
2021-02-15 17:58:35 -0300

    + Update Handle Errors Responses

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Post, Body, Get, Put, Delete, Res, HttpStatus, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Put,
+  Delete,
+  Res,
+  HttpStatus,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { UserDto } from '../dto/user-dto';
 import { UserService } from '../services/user.service';
 
@@ -8,47 +19,103 @@ export class UsersController {
 
   @Post()
   async create(@Body() createUser: UserDto, @Res() response) {
-    this.userService.createUser(createUser).then((data) => {
-      return response.status(HttpStatus.CREATED).json({ 'statusCode':HttpStatus.CREATED, 'statusMsg':'CREATED', 'data': [data]});
-    }).catch(err => {
-      return response.status(HttpStatus.BAD_REQUEST).json({ 'statusCode':HttpStatus.BAD_REQUEST, 'statusMsg':'BAD_REQUEST', 'error': err.message});
-    });
+    this.userService
+      .createUser(createUser)
+      .then(data => {
+        return response.status(HttpStatus.CREATED).json({
+          statusCode: HttpStatus.CREATED,
+          statusMsg: 'CREATED',
+          data: [data],
+        });
+      })
+      .catch(err => {
+        const { statusCode, message, error } = err.response;
+        return response.status(statusCode || HttpStatus.BAD_REQUEST).json({
+          statusCode: statusCode || HttpStatus.BAD_REQUEST,
+          statusMsg: error.toUpperCase(),
+          error: message,
+        });
+      });
   }
 
   @Get()
   getAll(@Res() response) {
-    this.userService.getAll().then(data => {
-      return response.status(HttpStatus.OK).json({ 'statusCode':HttpStatus.OK, 'statusMsg':'OK', 'data': data});
-    }).catch(err => {
-      return response.status(HttpStatus.BAD_REQUEST).json({ 'statusCode':HttpStatus.BAD_REQUEST, 'statusMsg':'BAD_REQUEST', 'error': err.message});
-    });
+    this.userService
+      .getAll()
+      .then(data => {
+        return response
+          .status(HttpStatus.OK)
+          .json({ statusCode: HttpStatus.OK, statusMsg: 'OK', data: data });
+      })
+      .catch(err => {
+        const { statusCode, message, error } = err.response;
+        return response.status(statusCode || HttpStatus.BAD_REQUEST).json({
+          statusCode: statusCode || HttpStatus.BAD_REQUEST,
+          statusMsg: error.toUpperCase(),
+          error: message,
+        });
+      });
   }
 
   @Get(':id')
   getOne(@Param('id') id, @Res() response) {
-    this.userService.getOne(id).then(data => {
-      return response.status(HttpStatus.OK).json({ 'statusCode':HttpStatus.OK, 'statusMsg':'OK', 'data': data});
-    }).catch(err => {
-      return response.status(HttpStatus.BAD_REQUEST).json({ 'statusCode':HttpStatus.BAD_REQUEST, 'statusMsg':'BAD_REQUEST', 'error': err.message});
-    });
+    this.userService
+      .getOne(id)
+      .then(data => {
+        if (data) {
+          return response
+            .status(HttpStatus.OK)
+            .json({ statusCode: HttpStatus.OK, statusMsg: 'OK', data: [data] });
+        } else {
+          throw new NotFoundException(`User ID ${id} not found`);
+        }
+      })
+      .catch(err => {
+        const { statusCode, message, error } = err.response;
+        return response.status(statusCode || HttpStatus.BAD_REQUEST).json({
+          statusCode: statusCode || HttpStatus.BAD_REQUEST,
+          statusMsg: error.toUpperCase(),
+          error: message,
+        });
+      });
   }
 
   @Put(':id')
-  update(@Body() updateUser: UserDto,@Param('id') id ,@Res() response) {
-    this.userService.update(id, updateUser).then(data => {
-      return response.status(HttpStatus.OK).json({ 'statusCode':HttpStatus.OK, 'statusMsg':'OK', 'data': data});
-    }).catch(err => {
-      return response.status(HttpStatus.BAD_REQUEST).json({ 'statusCode':HttpStatus.BAD_REQUEST, 'statusMsg':'BAD_REQUEST', 'error': err.message});
-    })
+  update(@Body() updateUser: UserDto, @Param('id') id, @Res() response) {
+    this.userService
+      .update(id, updateUser)
+      .then(data => {
+        return response
+          .status(HttpStatus.OK)
+          .json({ statusCode: HttpStatus.OK, statusMsg: 'OK', data: data });
+      })
+      .catch(err => {
+        const { statusCode, message, error } = err.response;
+        return response.status(statusCode || HttpStatus.BAD_REQUEST).json({
+          statusCode: statusCode || HttpStatus.BAD_REQUEST,
+          statusMsg: error.toUpperCase(),
+          error: message,
+        });
+      });
   }
 
   @Delete(':id')
-  delete(@Res() response,@Param('id') id) {
-    this.userService.remove(id).then(data => {
-      return response.status(HttpStatus.OK).json({ 'statusCode':HttpStatus.OK, 'statusMsg':'OK', 'data': data || `ID ${id} Eliminado`});
-    }).catch(err => {
-      return response.status(HttpStatus.BAD_REQUEST).json({ 'statusCode':HttpStatus.BAD_REQUEST, 'statusMsg':'BAD_REQUEST', 'error': err.message});
-    })
-   
+  delete(@Res() response, @Param('id') id) {
+    this.userService
+      .remove(id)
+      .then(() => {
+        return response.status(HttpStatus.OK).json({
+          statusCode: HttpStatus.OK,
+          statusMsg: 'OK',
+        });
+      })
+      .catch(err => {
+        const { statusCode, message, error } = err.response;
+        return response.status(statusCode || HttpStatus.BAD_REQUEST).json({
+          statusCode: statusCode || HttpStatus.BAD_REQUEST,
+          statusMsg: error.toUpperCase(),
+          error: message,
+        });
+      });
   }
 }
